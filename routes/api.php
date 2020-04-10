77488d4f4ab842c11857e4141d5d2680685404cb
César M. Cristóbal <cesar@callepuzzle.com>
2020-04-10 18:33:44 +0200

    Making web-api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,15 @@
 <?php
 
 /* @var $router \Illuminate\Support\Facades\Route */
+
 $router->get('/get-activities-table-format', function () use ($router) {
     return (new \App\Activities\ActivitiesTableFormat())->get();
+});
+
+$router->get('/get-activity-types', function () use ($router) {
+    return (new \App\Activities\Type())->all();
+});
+
+$router->get('/get-levels-by-type', function () use ($router) {
+    return (new \App\Activities\Level())->getLevelsByType();
 });
